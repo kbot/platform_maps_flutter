3c7e59260668ec2215322d18329427d5aa6fd143
Kevin Bloom <kevin@gogii.net>
2020-09-10 16:56:05 -0700

    add rotation to methods for creating platform markers from dart Marker

--- lib/src/marker.dart
+++ lib/src/marker.dart
@@ -210,6 +210,7 @@ class Marker {
                 _onAppleAnnotationDragEnd(latLng, marker.onDragEnd)
             : null,
         position: marker.position.appleLatLng,
+        rotation: marker.rotation,
       );
 
   static googleMaps.Marker googleMapsMarkerFromMarker(Marker marker) =>
@@ -228,6 +229,7 @@ class Marker {
                 _onGoogleMarkerDragEnd(latLng, marker.onDragEnd)
             : null,
         position: marker.position.googleLatLng,
+        rotation: marker.rotation,
       );
 
   static Set<appleMaps.Annotation> toAppleMapsAnnotationSet(
